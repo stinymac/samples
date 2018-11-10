9592311177bf4dc1d4db318180ee6e5d4bc10b46
machao <machao1@jianke.com>
2018-11-10 23:39:59 +0800

    update

---
+++ sample-spring-boot/src/main/java/org/mac/sample/spring/boot/config/propertiesbind/bean/PersonProperties.java
@@ -0,0 +1,204 @@
+/*
+ *      (             |"|           !!!       #   ___                             o
+ *      _)_          _|_|_       `  _ _  '    #  <_*_>             ,,,         ` /_\ '       __MMM__
+ *     (o o)         (o o)      -  (OXO)  -   #  (o o)            (o o)       - (o o) -       (o o)
+ * ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo--8---(_)--Ooo----ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
+ *
+ *
+ *
+ *                    !!!         |                             |"|            _             o          _     _
+ *    __MMM__      `  _ _  '      |.===.         ,,,,,         _|_|_         _|_|_        ` /_\ '     o' \,=./ `o
+ *     (o o)      -  (OXO)  -     {}o o{}       /(o o)\        (o o)         (o o)       - (o o) -       (o o)
+ * ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
+ *
+ *
+ * 虽不能至,心向往之。(Although it is not possible, my heart is longing for it.)
+ *
+ *       ___        |
+ *      /_\ `*      |.===.         ,,,,,
+ *     (o o)        {}o o{}       /(o o)\
+ * ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
+ *
+ */
+
+package org.mac.sample.spring.boot.config.propertiesbind.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A user object
+ *
+ * @ConfigurationProperties 标注的类与配置文件中的值绑定
+ *
+ * @auther mac
+ * @date 2018-11-10
+ */
+@Component
+@ConfigurationProperties(prefix = "sample.user")
+public class PersonProperties {
+
+    private String name;
+    private Integer age;
+    private boolean vip;
+    private Date birthday;
+
+    private IdentityCard identityCard;
+    private List<Address> addresses;
+    private Map<String,Object> properties;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public IdentityCard getIdentityCard() {
+        return identityCard;
+    }
+
+    public void setIdentityCard(IdentityCard identityCard) {
+        this.identityCard = identityCard;
+    }
+
+    public Map<String, Object> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Map<String, Object> properties) {
+        this.properties = properties;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonProperties{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", vip=" + vip +
+                ", birthday=" + birthday +
+                ", identityCard=" + identityCard.toString() +
+                ", properties=" + properties +
+                ", addresses=" + addresses.toString() +
+                '}';
+    }
+
+    public static  class IdentityCard {
+
+        private String code;
+        private String issuingAuthority;
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getIssuingAuthority() {
+            return issuingAuthority;
+        }
+
+        public void setIssuingAuthority(String issuingAuthority) {
+            this.issuingAuthority = issuingAuthority;
+        }
+
+        @Override
+        public String toString() {
+            return "IdentityCard{" +
+                    "code='" + code + '\'' +
+                    ", issuingAuthority='" + issuingAuthority + '\'' +
+                    '}';
+        }
+    }
+
+
+    public static class Address {
+
+        private String province;
+        private String city;
+        private String district;
+        private String detail;
+
+        public String getProvince() {
+            return province;
+        }
+
+        public void setProvince(String province) {
+            this.province = province;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getDistrict() {
+            return district;
+        }
+
+        public void setDistrict(String district) {
+            this.district = district;
+        }
+
+        public String getDetail() {
+            return detail;
+        }
+
+        public void setDetail(String detail) {
+            this.detail = detail;
+        }
+
+        @Override
+        public String toString() {
+            return "Address{" +
+                    "province='" + province + '\'' +
+                    ", city='" + city + '\'' +
+                    ", district='" + district + '\'' +
+                    ", detail='" + detail + '\'' +
+                    '}';
+        }
+    }
+}
--- sample-spring-boot/src/test/java/org/mac/sample/spring/boot/SampleSpringBootApplicationTests.java
+++ sample-spring-boot/src/test/java/org/mac/sample/spring/boot/SampleSpringBootApplicationTests.java
@@ -2,15 +2,21 @@ package org.mac.sample.spring.boot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mac.sample.spring.boot.config.propertiesbind.bean.PersonProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SampleSpringBootApplicationTests {
+	@Autowired
+	private PersonProperties user;
+
 
 	@Test
 	public void contextLoads() {
+		System.err.println(user);
 	}
 
 }
