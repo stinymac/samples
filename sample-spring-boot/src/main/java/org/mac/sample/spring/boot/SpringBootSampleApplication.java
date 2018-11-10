52e2adac9598fd9f0a2940028a3b7a1e018a252a
machao <machao1@jianke.com>
2018-11-10 21:17:10 +0800

    spring boot sample

---
+++ sample-spring-boot/src/main/java/org/mac/sample/spring/boot/SpringBootSampleApplication.java
@@ -0,0 +1,178 @@
+/*
+ *      (             |"|           !!!       #   ___                             o
+ *      _)_          _|_|_       `  _ _  '    #  <_*_>             ,,,         ` /_\ '       __MMM__
+ *     (o o)         (o o)      -  (OXO)  -   #  (o o)            (o o)       - (o o) -       (o o)
+ * ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo--8---(_)--Ooo----ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
+ *
+ *
+ *
+ *                    !!!         |                             |"|            _             o          _     _
+ *    __MMM__      `  _ _  '      |.===.         ,,,,,         _|_|_         _|_|_        ` /_\ '     o' \,=./ `o
+ *     (o o)      -  (OXO)  -     {}o o{}       /(o o)\        (o o)         (o o)       - (o o) -       (o o)
+ * ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
+ *
+ *
+ * 虽不能至,心向往之。(Although it is not possible, my heart is longing for it.)
+ *
+ *       ___        |
+ *      /_\ `*      |.===.         ,,,,,
+ *     (o o)        {}o o{}       /(o o)\
+ * ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
+ *
+ */
+package org.mac.sample.spring.boot;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationImportSelector;
+import org.springframework.boot.autoconfigure.AutoConfigurationMetadata;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.ConfigurationClassPostProcessor;
+import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.io.support.SpringFactoriesLoader;
+import org.springframework.core.type.AnnotationMetadata;
+
+import java.util.Collection;
+
+/**
+ * Spring Boot 应用主程序入口
+ *
+ * @see SpringBootApplication
+ *
+ * <pre>
+ *     @SpringBootConfiguration
+ *     @EnableAutoConfiguration
+ *     @ComponentScan(excludeFilters = {
+ * 		@Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+ * 		@Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class) })
+ * </pre>
+ *
+ * @see SpringBootConfiguration
+ * 其被@Configuration标注(即Spring的配置组件) 因此此类(@SpringBootApplication标注的类)
+ * 是Spring容器启动的配置类
+ *
+ * @see EnableAutoConfiguration 自动配置启用
+ *
+ * <pre>
+ *     @AutoConfigurationPackage
+ *
+ *     @Import(AutoConfigurationImportSelector.class)
+ * </pre>
+ *
+ * @AutoConfigurationPackage 被@Import(AutoConfigurationPackages.Registrar.class)标注
+ *
+ * 即向Spring容器中注册AutoConfigurationPackages.Registrar指定的BeanDefinition
+ *
+ * @see AutoConfigurationPackages.Registrar#registerBeanDefinitions(AnnotationMetadata, BeanDefinitionRegistry)
+ * <pre>
+ *     register(registry, new PackageImport(metadata).getPackageName());
+ * </pre>
+ *
+ * <pre>
+ *     PackageImport(AnnotationMetadata metadata) {
+ * 			this.packageName = ClassUtils.getPackageName(metadata.getClassName());
+ * 	   }
+ * </pre>
+ *
+ * @see AutoConfigurationPackages#register(BeanDefinitionRegistry, String...)
+
+ * 向容器中注册了ID为org.springframework.boot.autoconfigure.AutoConfigurationPackages 定义为
+ * <pre>
+ *     GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+ * 	   beanDefinition.setBeanClass(BasePackages.class);//org.springframework.boot.autoconfigure.AutoConfigurationPackages#BasePackages
+ * 	   beanDefinition.getConstructorArgumentValues().addIndexedArgumentValue(0,
+ * 					packageNames);//org.mac.sample.spring.boot
+ * </pre>
+ * 的Bean定义
+ *
+ * 所以@SpringBootApplication标注的启动配置入口类(SpringBootSampleApplication.class)所在的包及其子包中
+ * 所有的组件将注册到Spring容器中
+ *
+ * @Import(AutoConfigurationImportSelector.class)
+ * 向容器中注册(由ConfigurationClassPostProcessor处理 @see org.springframework.context.annotation.ConfigurationClassPostProcessor)
+ * AutoConfigurationImportSelector指定的Bean
+ *
+ * 在后置处理器ConfigurationClassPostProcessor执行ConfigurationClassParser时
+ * @see ConfigurationClassPostProcessor#postProcessBeanDefinitionRegistry(BeanDefinitionRegistry)
+ * 处理SpringBootConfiguration标注的类 当前为SpringBootSampleApplication.class
+ *
+ *
+ * @see org.springframework.context.annotation.ConfigurationClassParser#processImports(
+ *     org.springframework.context.annotation.ConfigurationClass,
+ *     org.springframework.context.annotation.ConfigurationClassParser.SourceClass, Collection, boolean)
+ *
+ * 配置类解析器收集到@SpringBootApplication上的@Import(AutoConfigurationPackages.Registrar.class)和@Import(AutoConfigurationImportSelector.class)
+ *
+ * @see org.springframework.context.annotation.ConfigurationClassParser.DeferredImportSelectorHandler#process()
+ *
+ * @see AutoConfigurationImportSelector.AutoConfigurationGroup#process(AnnotationMetadata, DeferredImportSelector)
+ *
+ * 其调用AutoConfigurationImportSelector#getAutoConfigurationEntry(AutoConfigurationMetadata, AnnotationMetadata)
+ * 返回自动装配的类全名称数组
+ *
+ *
+ * @see AutoConfigurationImportSelector
+ *     @see AutoConfigurationImportSelector#getAutoConfigurationEntry(AutoConfigurationMetadata, AnnotationMetadata)
+ *     <pre>
+ *      if (!isEnabled(annotationMetadata)) { // spring.boot.enableautoconfiguration == true?
+ * 		    return EMPTY_ENTRY;
+ * 		}
+ * 		AnnotationAttributes attributes = getAttributes(annotationMetadata);
+ * 		List<String> configurations = getCandidateConfigurations(annotationMetadata,
+ * 				attributes);
+ * 		configurations = removeDuplicates(configurations);
+ * 		Set<String> exclusions = getExclusions(annotationMetadata, attributes);
+ * 		checkExcludedClasses(configurations, exclusions);
+ * 		configurations.removeAll(exclusions);
+ * 		configurations = filter(configurations, autoConfigurationMetadata);
+ * 		fireAutoConfigurationImportEvents(configurations, exclusions);
+ * 		return new AutoConfigurationEntry(configurations, exclusions);
+ *     </pre>
+ *
+ * List<String> configurations = getCandidateConfigurations(annotationMetadata,attributes);返回全部的自动装配类候选集合
+ * @see AutoConfigurationImportSelector#getCandidateConfigurations(AnnotationMetadata, AnnotationAttributes)
+ * <pre>
+ *     List<String> configurations = SpringFactoriesLoader.loadFactoryNames(
+ *         getSpringFactoriesLoaderFactoryClass(), getBeanClassLoader());
+ * </pre>
+ *
+ * @see SpringFactoriesLoader#loadSpringFactories(ClassLoader)
+ * <pre>
+ *     //FACTORIES_RESOURCE_LOCATION = META-INF/spring.factories
+ *     //即类路径下的spring.factories文件中指定的自动装配类集合
+ *     Enumeration<URL> urls = (classLoader != null ?
+ * 					classLoader.getResources(FACTORIES_RESOURCE_LOCATION) :
+ * 					ClassLoader.getSystemResources(FACTORIES_RESOURCE_LOCATION));
+ * 	   result = new LinkedMultiValueMap<>();
+ * 	   while (urls.hasMoreElements()) {
+ * 	       URL url = urls.nextElement();
+ * 		   UrlResource resource = new UrlResource(url);
+ * 		   Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+ * 		   for (Map.Entry<?, ?> entry : properties.entrySet()) {
+ * 			   String factoryClassName = ((String) entry.getKey()).trim();
+ * 			   for (String factoryName : StringUtils.commaDelimitedListToStringArray((String) entry.getValue())) {
+ * 						result.add(factoryClassName, factoryName.trim());
+ * 			   }
+ * 		   }
+ *     }
+ * </pre>
+ * @see {@link spring-boot-autoconfigure-2.1.0.RELEASE-sources.jar!/META-INF/spring.factories}
+ *
+ * 所以SpringBoot默认支持的自动装配组件全部将被注册到Spring容器中
+ *
+ *
+ * @auther mac
+ * @date 2018-10-10
+ */
+@SpringBootApplication
+public class SpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleApplication.class, args);
+	}
+}
