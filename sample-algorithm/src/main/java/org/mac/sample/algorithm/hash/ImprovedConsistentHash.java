1671fe43da421ca33fe05f11790c86624d764ea8
machao <machao1@jianke.com>
2019-06-10 09:45:07 +0800

    update

--- sample-algorithm/src/main/java/org/mac/sample/algorithm/hash/ImprovedConsistentHash.java
+++ sample-algorithm/src/main/java/org/mac/sample/algorithm/hash/ImprovedConsistentHash.java
@@ -1,23 +1,14 @@
 /*
- *      (             |"|           !!!       #   ___                             o
- *      _)_          _|_|_       `  _ _  '    #  <_*_>             ,,,         ` /_\ '       __MMM__
- *     (o o)         (o o)      -  (OXO)  -   #  (o o)            (o o)       - (o o) -       (o o)
- * ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo--8---(_)--Ooo----ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
+ *          (          (
+ *          )\ )  (    )\   )  )     (
+ *  (  (   (()/( ))\( ((_| /( /((   ))\
+ *  )\ )\   ((_))((_)\ _ )(_)|_))\ /((_)
+ * ((_|(_)  _| (_))((_) ((_)__)((_|_))
+ * / _/ _ \/ _` / -_|_-< / _` \ V // -_)
+ * \__\___/\__,_\___/__/_\__,_|\_/ \___|
  *
- *
- *
- *                    !!!         |                             |"|            _             o          _     _
- *    __MMM__      `  _ _  '      |.===.         ,,,,,         _|_|_         _|_|_        ` /_\ '     o' \,=./ `o
- *     (o o)      -  (OXO)  -     {}o o{}       /(o o)\        (o o)         (o o)       - (o o) -       (o o)
- * ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
- *
- *
- * 虽不能至,心向往之。(Although it is not possible, my heart is longing for it.)
- *
- *       ___        |
- *      /_\ `*      |.===.         ,,,,,
- *     (o o)        {}o o{}       /(o o)\
- * ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
+ * 东隅已逝，桑榆非晚。(The time has passed,it is not too late.)
+ * 虽不能至，心向往之。(Although I can't, my heart is longing for it.)
  *
  */
 
@@ -26,7 +17,6 @@ package org.mac.sample.algorithm.hash;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -44,7 +34,9 @@ public class ImprovedConsistentHash {
             "192.168.36.100:6379",
             "192.168.36.101:6379",
             "192.168.36.102:6379",
-            "192.168.36.103:6379")
+            "192.168.36.103:6379",
+            "192.168.36.104:6379"
+            )
     );
 
     /**key表示服务器的hash值，value表示服务器的名称*/
